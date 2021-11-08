428a2416868af58fcfb30509293b83f864154d03
Timm Preetz <timm@preetz.us>
2021-11-08 22:03:34 +0100

    Overlay `AppBar` as in production scenario

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,38 +1,39 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(
-      Column(
-        children: [
-          SizedBox(
-            height: 300,
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                    Color.fromRGBO(255, 221, 0, 1),
-                    Color.fromRGBO(246, 178, 34, 1),
-                  ],
+      MaterialApp(
+        home: Scaffold(
+          body: Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color.fromRGBO(255, 221, 0, 1),
+                      Color.fromRGBO(246, 178, 34, 1),
+                    ],
+                  ),
                 ),
               ),
-            ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: AppBar(
+                  backgroundColor: Colors.transparent,
+                  systemOverlayStyle: SystemUiOverlayStyle.light,
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
 
-    expect(find.byType(Container), matchesGoldenFile("gradient.png"));
+    expect(find.byType(Scaffold), matchesGoldenFile("gradient.png"));
   });
 }
