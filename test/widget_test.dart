46fe839ab389c83889e6bb178c2272b83c1ab24f
Timm Preetz <timm@preetz.us>
2021-11-08 21:37:35 +0100

    Test on M1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_gradient_test_mac/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      Column(
+        children: [
+          SizedBox(
+            height: 300,
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color.fromRGBO(255, 221, 0, 1),
+                    Color.fromRGBO(246, 178, 34, 1),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(Container), matchesGoldenFile("gradient.png"));
   });
 }
