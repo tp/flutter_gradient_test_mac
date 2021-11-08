1387b186acd179e61266f4a2e3d71941a6c9f636
Timm Preetz <timm@preetz.us>
2021-11-08 22:07:35 +0100

    Add theme to gradient

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,28 +7,31 @@ void main() {
     await tester.pumpWidget(
       MaterialApp(
         home: Scaffold(
-          body: Stack(
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [
-                      Color.fromRGBO(255, 221, 0, 1),
-                      Color.fromRGBO(246, 178, 34, 1),
-                    ],
+          body: Theme(
+            data: darkTheme,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                        Color.fromRGBO(255, 221, 0, 1),
+                        Color.fromRGBO(246, 178, 34, 1),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.topCenter,
-                child: AppBar(
-                  backgroundColor: Colors.transparent,
-                  systemOverlayStyle: SystemUiOverlayStyle.light,
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: AppBar(
+                    backgroundColor: Colors.transparent,
+                    systemOverlayStyle: SystemUiOverlayStyle.light,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -37,3 +40,12 @@ void main() {
     expect(find.byType(Scaffold), matchesGoldenFile("gradient.png"));
   });
 }
+
+final ThemeData darkTheme = ThemeData(
+  appBarTheme: ThemeData.dark().appBarTheme.copyWith(
+        color: Colors.transparent,
+        shadowColor: Colors.transparent,
+        centerTitle: false,
+        brightness: Brightness.dark,
+      ),
+);
